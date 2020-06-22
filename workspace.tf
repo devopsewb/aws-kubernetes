6858685325b991f6b684177bc207fb7b69b1d4cf
devopsewb <53023609+devopsewb@users.noreply.github.com>
2020-06-22 11:23:26 -0700

    Update workspace.tf

--- workspace.tf
+++ workspace.tf
@@ -1,4 +1,10 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+
 resource "aws_cloud9_environment_ec2" "example" {
   instance_type = "t2.micro"
   name          = "example-env"
+  region        = "us-west-2"
 }
