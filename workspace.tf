597ce02718a57ca7228cb6d3bfc09574822fbceb
devopsewb <53023609+devopsewb@users.noreply.github.com>
2020-06-22 12:08:20 -0700

    Update workspace.tf

--- workspace.tf
+++ workspace.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 
-resource "aws_cloud9_environment_ec2" "example" {
+resource "aws_cloud9_environment_ec2" "example2" {
   instance_type = "t2.micro"
   name          = "example-env"
   subnet_id     = "subnet-080a96789927f2d9f"
